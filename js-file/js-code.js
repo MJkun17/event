e224df0375b8d32687f840f7b412f61fa19137f7
MJkun17 <markjaydaria@gmail.com>
2022-05-04 01:28:03 +0800

    Video
    ye

--- js-file/js-code.js
+++ js-file/js-code.js
@@ -1,31 +1,33 @@
 //back to top btn
 
-    //Get the button
-    var mybutton = document.getElementById("myBtn");
-    
-    // When the user scrolls down 20px from the top of the document, show the button
-    window.onscroll = function() {scrollFunction()};
-    
-    function scrollFunction() {
-      if (document.body.scrollTop > 800 || document.documentElement.scrollTop > 800) {
-        mybutton.style.display = "block";
-      } else {
-        mybutton.style.display = "none";
-      }
-    }
-    
-    // When the user clicks on the button, scroll to the top of the document
-    function topFunction() {
-      document.body.scrollTop = 0;
-      document.documentElement.scrollTop = 0;
-    }
+//Get the button
+var mybutton = document.getElementById("myBtn");
 
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
 
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 800 ||
+    document.documentElement.scrollTop > 800
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
 //for slide in js
 
-
-      const scrollElements = document.querySelectorAll(".js-scroll");
+const scrollElements = document.querySelectorAll(".js-scroll");
 
 const elementInView = (el, dividend = 1) => {
   const elementTop = el.getBoundingClientRect().top;
@@ -57,12 +59,27 @@ const handleScrollAnimation = () => {
     if (elementInView(el, 1.25)) {
       displayScrollElement(el);
     } else if (elementOutofView(el)) {
-      hideScrollElement(el)
+      hideScrollElement(el);
     }
-  })
-}
+  });
+};
 
-window.addEventListener("scroll", () => { 
+window.addEventListener("scroll", () => {
   handleScrollAnimation();
 });
-    
+
+// script for video gallery
+var video = document.querySelectorAll("video");
+
+video.forEach((play) =>
+  play.addEventListener("click", () => {
+    play.classList.toggle("active");
+
+    if (play.paused) {
+      play.play();
+    } else {
+      play.pause();
+      play.currentTime = 0;
+    }
+  })
+);
